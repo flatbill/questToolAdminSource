66ec7507d805dd821255c11c491a840737adf326
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-11-29 19:35:11 -0500

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Questool Admin';
+  title = 'Questool Admin'
+  showQnc = true
+  showQncWwg = false
+  subset = '?'
+  meow = 'mmmeeeooowww'
+  subsetqncwwg = '??subsetqncwwg??'
+
+  showHideQnc(){
+    console.log('running showHideQnc')
+    if (this.showQnc) {
+      this.showQnc = false
+    } else {
+      this.showQnc = true
+    }
+  } // end showHideQnc
+
+  showHideQncWwg(){
+    console.log('running showHideQncWwg')
+    if (this.showQncWwg) {
+      this.showQncWwg = false
+    } else {
+      this.showQncWwg = true
+    }
+  } // end showHideQnc
+
+  setSubsetOut(event){
+    console.log('running setSubsetOut')
+    this.subset = event
+    this.subsetqncwwg = this.subset
+  } // end setSubsetOut
+   
+
 }
--- app.module.ts
+++ app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QncComponent } from './qnc/qnc.component';
+import { QncwwgComponent } from './qncwwg/qncwwg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QncComponent
+    QncComponent,
+    QncwwgComponent
   ],
   imports: [
     BrowserModule
